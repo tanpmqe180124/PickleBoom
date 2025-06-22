5074bd61e26c99d749017898b7e7184da9230922
tanpmqe180124 <155402912+tanpmqe180124@users.noreply.github.com>
2025-06-22 19:53:41 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,14 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
+import { defineConfig } from 'vite';
+import path from 'path'
 
 export default defineConfig({
   base: '/',
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'public/_redirects',
-          dest: '.'
-        }
-      ]
-    }),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
 });
+
